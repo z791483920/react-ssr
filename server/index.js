dc156e79ad5c4b47a599efae335badad8589f530
yang09 <zengyang194@163.com>
2019-07-26 10:32:31 +0800

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,16 @@
+// const ignore = () => {
+//     const extensions = [
+//     // '.css',
+//     // '.scss',
+//     // '.less',
+//     // '.scss',
+//         '.png',
+//         '.jpg',
+//         '.gif'
+//     ];
+//     for (let i = 0, len = extensions.length; i < len; i++) {
+//         require.extensions[extensions[i]] = () => null;
+//     }
+// };
+// ignore();
+require('./app.js');
