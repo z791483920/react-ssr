20041845a3c0e26235c4c099179b29a09a38d937
yang09 <zengyang194@163.com>
2019-07-26 10:27:28 +0800

    Create Login.jsx

---
+++ client/pages/Login.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import _ from 'lodash';
+import './login.css';
+
+class Login extends React.Component {
+    render() {
+        return (
+            <div className="login">
+                <div>登陆页面</div>
+                <button
+                    type="button"
+                    onClick={() => {
+                        alert('23123123');
+                    }}
+                >
+          click
+                </button>
+
+                <a
+                    onClick={() => {
+                        const aa = _.cloneDeep({ a: 1 });
+                        this.props.history.push('/');
+                    }}
+                >
+          去home
+                </a>
+            </div>
+        );
+    }
+}
+
+export default Login;
