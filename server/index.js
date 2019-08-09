98ef7038544361020ca4915b9bdffd75673044fd
yang09 <zengyang194@163.com>
2019-08-09 17:04:51 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,16 +1,1 @@
-// const ignore = () => {
-//     const extensions = [
-//     // '.css',
-//     // '.scss',
-//     // '.less',
-//     // '.scss',
-//         '.png',
-//         '.jpg',
-//         '.gif'
-//     ];
-//     for (let i = 0, len = extensions.length; i < len; i++) {
-//         require.extensions[extensions[i]] = () => null;
-//     }
-// };
-// ignore();
 require('./app.js');
