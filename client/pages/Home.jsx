cac6d36e969bff2ca6db089e81dd2230c69af479
yang09 <zengyang194@163.com>
2019-08-09 17:00:57 +0800

    Update Home.jsx

--- client/pages/Home.jsx
+++ client/pages/Home.jsx
@@ -1,31 +1,84 @@
 import React from 'react';
-import './Home.css';
+import Header from '../components/Header';
+import './index.css';
+import { Provider, Consumer } from '../components/Test';
+import Context from '../components/Context';
+import TestReducer from '../components/TestReducer';
+import { Button } from 'antd';
 
 class Home extends React.Component {
   state = {
-      aaa: ''
+      userInfo: {
+          name: 'lxfriday',
+          age: 24
+      },
+      school: 'hzzz'
+  };
+
+  listRef = React.createRef();
+
+  aaa = 13123;
+
+  static fetch = () => '1111';
+  // static getDerivedStateFromProps(nextProps, prevState) {
+  //     console.log(nextProps, 'nextProps');
+  //     console.log(prevState, 'prevState');
+  //     return null;
+  // }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+      console.log('getSnapshotBeforeUpdate');
+      return this.aaa;
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+      console.log(snapshot, ';1213snap');
+  }
+
+  static getDerivedStateFromError(error) {
+      return { hasError: true };
+  }
+
+  handleChangeUserInfo = () => {
+      const { userInfo } = this.state;
+      userInfo.sex = 'male';
+      this.setState({ userInfo });
+  };
+
+  handleChangeSchool = () => {
+      this.setState({ school: 'zzzh' });
   };
 
   render() {
+      const { userInfo, school, hasError } = this.state;
       return (
-          <div className="home" style={{ color: 'green', fontSize: '31px' }}>
-              <div>This is sanyuan</div>
-              <button
-                  type="button"
-                  onClick={() => {
-                      alert('13');
-                  }}
-              >
-          click
-              </button>
-              <div>dgfasd</div>
-              <a
-                  onClick={() => {
-                      this.props.history.push('/login');
-                  }}
-              >
-          去login
-              </a>
+          <div ref={this.listRef}>
+              <Header />
+              {/* <div>
+                  <button onClick={this.handleChangeUserInfo}>change userInfo</button>
+                  <button onClick={this.handleChangeSchool}>change school</button>
+                  <button
+                      onClick={() => {
+                          this.aaa += 1;
+                      }}
+                  >
+            change aaa
+                  </button>
+
+                  <br />
+                  {JSON.stringify(userInfo)}
+                  <br />
+              </div> */}
+              <TestReducer />
+              <div>
+                  <Button
+                      onClick={() => {
+                          this.props.history.push('/login');
+                      }}
+                  >
+            去login
+                  </Button>
+              </div>
           </div>
       );
   }
